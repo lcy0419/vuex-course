5208116c4fbb08d8373465f76fec775bce55be2d
lcy0419 <1169337554@qq.com>
2019-09-02 20:11:18 +0800

    'lesson-1'

--- src/store.js
+++ src/store.js
@@ -5,6 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    //属性
+    count: 0,
+    todos: [
+      { id: 1, title: "todos item 1", completed: false },
+      { id: 2, title: "todos item 2", completed: false },
+      { id: 3, title: "todos item 3", completed: false }
+
+    ]
+  },
+  getters: {
 
   },
   mutations: {
